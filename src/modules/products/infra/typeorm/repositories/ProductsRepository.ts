3ac94696701b2bee1d0b428c6a2efce8348f3da0
Emanuel Ferreira <contatoferreirads@gmail.com>
2020-05-29 19:32:02 -0300

    first commit

---
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,78 @@
+import { getRepository, Repository, In } from 'typeorm';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
+
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const productsIds = products.map(product => product.id);
+
+    const productList = await this.ormRepository.find({
+      where: {
+        id: In(productsIds),
+      },
+    });
+
+    return productList;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    const productsData = await this.findAllById(products);
+
+    for (const product of productsData) {
+      const updatedProduct = products.find(
+        toUpdateProduct => toUpdateProduct.id === product.id,
+      );
+
+      if (updatedProduct) {
+        product.quantity -= updatedProduct.quantity;
+
+        await this.ormRepository.save(product);
+      }
+    }
+
+    return productsData;
+  }
+}
+
+export default ProductsRepository;
